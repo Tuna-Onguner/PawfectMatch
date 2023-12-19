ce1cf6abc00f09861d01fadd090d98b230a52ec1
Hasan Ege <hasan.tunc@ug.bilkent.edu.tr>
2023-12-19 16:38:01 +0300

    Merged frontend

--- frontend/admin/models/user-models.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    role: string;
-    createdAt: Date;
-}
--- frontend/frontendapp/src/app/app.routes.ts
+++ frontend/frontendapp/src/app/app.routes.ts
@@ -17,6 +17,34 @@ import {ApplicationsViewComponent} from "./applications-view/applications-view.c
 import {ReservationsViewComponent} from "./reservations-view/reservations-view.component";
 import {ExaminationsComponent} from "./examinations/examinations.component";
 import {OverseeingPageComponent} from "./overseeing-page/overseeing-page.component";
+import {MakeAgreementsComponent} from "./make-agreements/make-agreements.component";
+import {DetailAgreementsComponent} from "./detail-agreements/detail-agreements.component";
+import {PublishSchedulesComponent} from "./publish-schedules/publish-schedules.component";
+import {SchedulingRequestsComponent} from "./scheduling-requests/scheduling-requests.component";
+import {DetailAppointmentsComponent} from "./detail-appointments/detail-appointments.component";
+import {UploadExamResultsComponent} from "./upload-exam-results/upload-exam-results.component";
+import {DetailExamResultsComponent} from "./detail-exam-results/detail-exam-results.component";
+import {HomeComponent} from "./home/home.component";
+import {DetailPetComponent} from "./detail-pet/detail-pet.component";
+import {AddPetComponent} from "./add-pet/add-pet.component";
+import {AdoptionApplicationsComponent} from "./adoption-applications/adoption-applications.component";
+import {DetailAdoptionApplicationComponent} from "./detail-adoption-application/detail-adoption-application.component";
+import {ApplyForGrantsComponent} from "./apply-for-grants/apply-for-grants.component";
+import {FindVeterinariansComponent} from "./find-veterinarians/find-veterinarians.component";
+import {DetailVetAgreementComponent} from "./detail-vet-agreement/detail-vet-agreement.component";
+import {ReceiveDonationsComponent} from "./receive-donations/receive-donations.component";
+import {RequestOverseeComponent} from "./request-oversee/request-oversee.component";
+import {DetailOverseeComponent} from "./detail-oversee/detail-oversee.component";
+import {UserAdministrationComponent} from "./user-administration/user-administration.component";
+import {DetailUserComponent} from "./detail-user/detail-user.component";
+import {BloggerApplicationsComponent} from "./blogger-applications/blogger-applications.component";
+import {DetailBlogApplicationsComponent} from "./detail-blog-applications/detail-blog-applications.component";
+import {ExpertApplicationsComponent} from "./expert-applications/expert-applications.component";
+import {DetailExpertApplicationsComponent} from "./detail-expert-applications/detail-expert-applications.component";
+import {GranteeApplicationsComponent} from "./grantee-applications/grantee-applications.component";
+import {DetailGranteeApplicationsComponent} from "./detail-grantee-applications/detail-grantee-applications.component";
+import {TransferringDonationsComponent} from "./transferring-donations/transferring-donations.component";
+import {DetailTransferComponent} from "./detail-transfer/detail-transfer.component";
 
 export const routes: Routes = [
   {path: 'register', component: RegisterComponentComponent},
@@ -40,6 +68,64 @@ export const routes: Routes = [
       {path: 'oversight-view', component: OverseeingPageComponent},
     ]
   },
+  {path: 'make-agreements',
+    component: MakeAgreementsComponent,
+    children: [
+      {path: 'detail-agreements', component: DetailAgreementsComponent},
+    ]
+    },
+    { path: '', redirectTo: '/make-agreements', pathMatch: 'full' },
+    { path: 'publish-schedule', component: PublishSchedulesComponent },
+    { path: 'scheduling-request', component: SchedulingRequestsComponent,
+      children: [
+        {path: 'detail-appointmes', component: DetailAppointmentsComponent},
+      ] },
+    {path: 'upload-exam-results', component: UploadExamResultsComponent,
+      children: [
+      {path: 'detail-exam-results', component: DetailExamResultsComponent},
+    ]},
+  {path: 'home-organization', component: HomeComponent,
+        children: [
+            {path:'detail-pet',component: DetailPetComponent}]
+        },
+    { path: '', redirectTo: '/home-organization', pathMatch: 'full' },
+    {path: 'add-pet', component: AddPetComponent},
+    {path: 'adoption-applications', component: AdoptionApplicationsComponent,
+    children: [
+        {path:'detail-adoption-application',component: DetailAdoptionApplicationComponent}]
+    },
+    {path: 'apply-for-grants', component: ApplyForGrantsComponent},
+    {path: 'find-veterinarians', component: FindVeterinariansComponent,
+    children: [
+        {path:'detail-vet-agreement',component: DetailVetAgreementComponent}]
+    },
+    {path: 'receive-donations', component: ReceiveDonationsComponent},
+    {path: 'request-oversee', component: RequestOverseeComponent,
+    children: [
+        {path:'detail-oversee',component: DetailOverseeComponent}]
+    },
+  {path: 'user-administration', component: UserAdministrationComponent,
+    children: [
+        {path: 'detail-user', component: DetailUserComponent},
+      ]},
+    { path: '', redirectTo: '/user-administration', pathMatch: 'full' },
+    {path: 'blogger-applications', component: BloggerApplicationsComponent,
+    children: [
+        {path: 'detail-blog-applications', component: DetailBlogApplicationsComponent},
+      ]},
+    { path: '', redirectTo: '/user-administration', pathMatch: 'full' },
+    {path: 'expert-applications', component: ExpertApplicationsComponent,
+        children: [
+        {path: 'detail-expert-applications', component: DetailExpertApplicationsComponent},
+      ]},
+    {path: 'grantee-applications', component: GranteeApplicationsComponent,
+    children: [
+        {path: 'detail-grantee-applications', component: DetailGranteeApplicationsComponent},
+      ]},
+    {path: 'transferring-donations', component: TransferringDonationsComponent,
+    children: [
+      {path: 'detail-transfer', component: DetailTransferComponent},
+    ]},
 ];
 
 @NgModule({
