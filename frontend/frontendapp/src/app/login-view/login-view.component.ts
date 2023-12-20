2a785238eb1ea391ee825e65add8c9fd39fdd164
HEgeTunc <hasan.tunc@ug.bilkent.edu.tr>
2023-12-20 15:55:17 +0300

    Login page added

---
+++ frontend/frontendapp/src/app/login-view/login-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-view.component.html',
+  styleUrl: './login-view.component.css'
+})
+export class LoginViewComponent {
+
+}
