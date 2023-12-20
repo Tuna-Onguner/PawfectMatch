4853b6d661f0b8604190246adc451a9f5f7248a7
Anıl İlağa <anil.ilaga@ug.bilkent.edu.tr>
2023-12-20 16:43:29 +0300

    Created admin model and Implemented Auth functions

---
+++ backend/PawfectMatch/authentication/models/Backends.py
@@ -0,0 +1,35 @@
+from typing import Any
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.http.request import HttpRequest
+from django.db import connection
+from .User import User
+import pdb
+
+
+class MyBackend(BaseBackend):
+    ##Define an auth method to check if the given email and password user exists in the db
+    @staticmethod
+    def authenticate(request: HttpRequest, email: str, password: str) -> Any:
+        cursor = connection.cursor()
+        pdb.set_trace()
+
+        # SQL CODE TO CHECK IF USER EXISTS
+        cursor.execute(
+            "SELECT * FROM User WHERE email = %s AND password = %s", [email, password]
+        )
+        row = cursor.fetchone()
+        if row is None:
+            return None
+        else:
+            # Return the user model if the user exists
+            return User.objects.get(email=email)
+
+    ##Define a login method to login the user
+    @staticmethod
+    def login(request: HttpRequest, user: AbstractBaseUser) -> None:
+        request.session["user_id"] = user.user_id
+
+    @staticmethod
+    def logout(request: HttpRequest) -> None:
+        request.session.flush()
--- backend/PawfectMatch/roles/models/__init__.py
+++ backend/PawfectMatch/roles/models/__init__.py
@@ -3,3 +3,4 @@ from .adoption_organization import *
 from .blogger import *
 from .expert import *
 from .veterinarian import *
+from .admin import *
