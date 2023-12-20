6c02f8a91addc7930de362a3d63b54ba90a42f34
HEgeTunc <hasan.tunc@ug.bilkent.edu.tr>
2023-12-20 17:45:08 +0300

    Services are started to be added

--- frontend/frontendapp/src/app/login-view/login-view.component.ts
+++ frontend/frontendapp/src/app/login-view/login-view.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router, RouterLink} from "@angular/router";
+import {AuthenticationService} from "../../services/authentication-services";
 
 @Component({
   selector: 'app-login-view',
@@ -17,13 +18,14 @@ import {Router, RouterLink} from "@angular/router";
     ReactiveFormsModule,
     RouterLink
   ],
+  providers: [AuthenticationService],
   templateUrl: './login-view.component.html',
   styleUrl: './login-view.component.css'
 })
 export class LoginViewComponent {
     loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthenticationService) {
     this.loginForm = this.formBuilder.group({
       username: '',
       password: ''
@@ -32,16 +34,39 @@ export class LoginViewComponent {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ["USERNAME", Validators.required],
+      password: ["PASSWORD", Validators.required]
     });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      // Implement your logic to login the user here
-      // If login is successful, redirect the user to the desired page
-      // this.router.navigate(['/desired-page']);
+    const usernameControl = this.loginForm.get('username');
+    const passwordControl = this.loginForm.get('password');
+
+    const username = usernameControl ? usernameControl.value : null;
+    const password = passwordControl ? passwordControl.value : null;
+    this.authService.login(username, password).subscribe({
+    next: response => {
+      if (response.status === 200) {
+      // Handle the successful response
+      const data = response.body;
+      if (data.role === 'admin') {
+        this.router.navigate(['/admin-page']).then(r => console.log(r));
+      } else if (data.role === 'user') {
+        this.router.navigate(['/user-page']).then(r => console.log(r));
+      } else {
+        this.router.navigate(['/default-page']);
+      }
+    } else {
+      // Handle other status codes
+        this.router.navigate(['/register']);
     }
+    },
+    error: error => {
+      // Handle the error
+    }
+});
+  }
   }
 }
